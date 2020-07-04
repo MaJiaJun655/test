a075aaabc873c2d888c2e0b123ac7a271cc71f45
Administrator <Administrator@Win10-2020MZZAB>
2020-07-04 15:49:32 +0800

    注释更新

--- y2abc6/src/main/java/com/accp/Y2abcApplication.java
+++ y2abc6/src/main/java/com/accp/Y2abcApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hello
 @SpringBootApplication
 @MapperScan("com.accp.dao")
 public class Y2abcApplication {
